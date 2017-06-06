8f0d4dcc89b838e17859c9d6a6c5b1f63b2e1079
Rafael805 <rafaelhernandez7232@gmail.com>
2017-06-06 11:18:26 -0700

    New

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-var numberDisplay = document.getElementById("numberDisplay");
 var messageDisplay = document.querySelector("#message");
-var num1 = document.getElementById("num1").innerHTML=Math.floor((Math.random() * 10) + 1);
-var num2 = document.getElementById("num2").innerHTML=Math.floor((Math.random() * 10) + 1);
 var resetButton = document.querySelector("#reset");
-var modeButtons = document.querySelectorAll(".mode");
 var checkAnswer = document.querySelector('input[type=text]');
 var btn = document.querySelector('input[type=button][value=check]');
+var num1Display = document.getElementById("num1");
+var num2Display= document.getElementById("num2");
+var num1= Math.floor((Math.random() * 10) + 1);
+var num2 =Math.floor((Math.random() * 10) + 1);
 var answer = num1 + num2;
-
+num1Display.textContent = num1;
+num2Display.textContent = num2;
 
 btn.onclick = function() {
         if (checkAnswer.value == answer) {
@@ -17,13 +18,13 @@ btn.onclick = function() {
                 messageDisplay.textContent = "You are incorrect";
                 messageDisplay.style.color = "red";
         }
+
+        document.querySelector('input[type=text]').value = "";
+        document.getElementById('num1').innerHTML = "";
+        document.getElementById('num2').innerHTML = "";
+        num1 = Math.floor((Math.random() * 10) + 1);
+        num2 = Math.floor((Math.random() * 10) + 1);
+        document.getElementById('num1').innerHTML = num1;
+        document.getElementById('num2').innerHTML = num2;
+        answer = num1 + num2;
 };
-
-
-
-// resetButton.addEventListener("click",  function() {
-//         resetButton.textContent = "New Numbers";
-//         messageDisplay.textContent = "";
-//         num1 = document.getElementById("num1").innerHTML= Math.floor((Math.random() * 10) + 1);
-//         num2 = document.getElementById("num2").innerHTML= Math.floor((Math.random() * 10) + 1);
-// });

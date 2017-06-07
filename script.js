2ac7b075d8b2f5268077b7e556989d0a49e8dced
Rafael805 <rafaelhernandez7232@gmail.com>
2017-06-06 19:35:59 -0700

    Updates
    Updates

--- script.js
+++ script.js
@@ -7,18 +7,26 @@ var num2Display= document.getElementById("num2");
 var num1= Math.floor((Math.random() * 10) + 1);
 var num2 =Math.floor((Math.random() * 10) + 1);
 var header = document.querySelector("#h1");
-var answer = num1 + num2;
+var pointsDisplay = document.getElementById("points");
 var points = 0;
+var answer = num1 + num2;
 num1Display.textContent = num1;
 num2Display.textContent = num2;
 
 $(function() {
+
+        $("input").keydown(function(event) {
+                if(event.keyCode == 13) {
+                        $(".ui.primary.button").trigger("click");
+                }
+        });
+
         $(".ui.primary.button").on("click", function() {
                 if (checkAnswer.value == answer) {
                         messageDisplay.textContent = "You are correct";
                         messageDisplay.style.color = "green";
                         points++;
-                        console.log(points);
+                        pointsDisplay.textContent = points;
                         reset();
                 } else {
                         messageDisplay.textContent = "Try again";
@@ -41,11 +49,17 @@ $(function() {
         var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
 
         function timer() {
-                  count--;
-                  if (count <= 0) {
-                     clearInterval(counter);
-                     return;
-                  }
+                count--;
+                if (count < 0) {
+                        clearInterval(counter);
+                        return;
+                }
+                else if(count === 0) {
+                        alert("Game Over");
+                        points = 0;
+                        pointsDisplay.textContent = 0;
+                        count=30;
+                }
                  document.getElementById("timer").textContent = count + " secs";
         }
 

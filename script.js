96e23e25e0dd2783c0fe76f96d1a63528a1b0420
Rafael805 <rafaelhernandez7232@gmail.com>
2017-06-06 18:42:26 -0700

    Updates

--- script.js
+++ script.js
@@ -6,25 +6,47 @@ var num1Display = document.getElementById("num1");
 var num2Display= document.getElementById("num2");
 var num1= Math.floor((Math.random() * 10) + 1);
 var num2 =Math.floor((Math.random() * 10) + 1);
+var header = document.querySelector("#h1");
 var answer = num1 + num2;
+var points = 0;
 num1Display.textContent = num1;
 num2Display.textContent = num2;
 
-btn.onclick = function() {
-        if (checkAnswer.value == answer) {
-                messageDisplay.textContent = "You are correct";
-                messageDisplay.style.color = "green";
-        } else {
-                messageDisplay.textContent = "You are incorrect";
-                messageDisplay.style.color = "red";
+$(function() {
+        $(".ui.primary.button").on("click", function() {
+                if (checkAnswer.value == answer) {
+                        messageDisplay.textContent = "You are correct";
+                        messageDisplay.style.color = "green";
+                        points++;
+                        console.log(points);
+                        reset();
+                } else {
+                        messageDisplay.textContent = "Try again";
+                        messageDisplay.style.color = "red";
+                }
+        });
+
+        function reset() {
+                        document.querySelector('input[type=text]').value = "";
+                        document.getElementById('num1').textContent = "";
+                        document.getElementById('num2').textContent = "";
+                        num1 = Math.floor((Math.random() * 10) + 1);
+                        num2 = Math.floor((Math.random() * 10) + 1);
+                        document.getElementById('num1').textContent = num1;
+                        document.getElementById('num2').textContent = num2;
+                        answer = num1 + num2;
+        };
+
+        var count = 30;
+        var counter=setInterval(timer, 1000); //1000 will  run it every 1 second
+
+        function timer() {
+                  count--;
+                  if (count <= 0) {
+                     clearInterval(counter);
+                     return;
+                  }
+                 document.getElementById("timer").textContent = count + " secs";
         }
 
-        document.querySelector('input[type=text]').value = "";
-        document.getElementById('num1').innerHTML = "";
-        document.getElementById('num2').innerHTML = "";
-        num1 = Math.floor((Math.random() * 10) + 1);
-        num2 = Math.floor((Math.random() * 10) + 1);
-        document.getElementById('num1').innerHTML = num1;
-        document.getElementById('num2').innerHTML = num2;
-        answer = num1 + num2;
-};
+});
